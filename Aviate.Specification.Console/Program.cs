66ea854a1d377cc040104903fbcbb008188b90fc
Самохвалов Кирилл <mrson1qofficial@gmail.com>
2019-03-20 22:44:26 +0300

    Created solution.

---
+++ Aviate.Specification.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace Aviate.Specification.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
